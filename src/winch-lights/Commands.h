72c8e7b2654b14c5c8b0a6f462181d9720314323
timt <ttennant@gmail.com>
2023-05-06 23:06:32 +0100

    Makes sure Commands.h only gets defined once

--- src/winch-lights/Commands.h
+++ src/winch-lights/Commands.h
@@ -1,8 +1,9 @@
-#ifndef COMMANDS_H
+#ifndef WINCH_LIGHTS_COMMANDS_H
+#define WINCH_LIGHTS_COMMANDS_H
 static const String NO_COMMAND    = "NONE"; //0 or empty
 static const String TAKE_UP_SLACK = "TAKE_UP_SLACK"; //2
 static const String ALL_OUT       = "ALL_OUT_ALL_OUT"; //1
 static const String STOP          = "STOP_STOP_STOP"; //3
 static const String CANCEL_STOP   = "CANCEL_STOP"; //4
 
-#endif
+#endif //WINCH_LIGHTS_COMMANDS_H
