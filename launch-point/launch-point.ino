9a2bbe4fe9e7333ee7721e6a86e6ad209d35c9ef
timt <ttennant@gmail.com>
2023-05-06 23:08:58 +0100

    Update reference to LaunchPointLights after rename

--- launch-point/launch-point.ino
+++ launch-point/launch-point.ino
@@ -4,14 +4,14 @@ int txId = 0;
 int interval = 100;
 Comms comms("launch-point", "winch", "ESGC");
 Buttons buttons;
-LedLights ledLights(interval);
+LaunchPointLeds launchPointLeds(interval);
 
 
 void setup() {
     Serial.begin(9600);
     comms.setup();
     buttons.setup();
-    ledLights.setup();
+    launchPointLeds.setup();
     Serial.println();
     Serial.print("Launch point started.");
 }
@@ -20,15 +20,15 @@ void loop() {
     String command = buttons.checkButtonPress();
     if (command != NO_COMMAND) {
         comms.sendMessage(command, txId++);
-        ledLights.setStateTransmitting();
+        launchPointLeds.setStateTransmitting();
     }
     delay(interval);
     ReceiveResult result = comms.receiveMessage();
     if (result.exists()) {
         Serial.println("Received command: " + result._command + ", txId: " + result._txId);
-        ledLights.setStateReceiving(result._command);
+        launchPointLeds.setStateReceiving(result._command);
     } else {
-        ledLights.checkBatteryAndReset();
+        launchPointLeds.checkBatteryAndReset();
     }
 }
 
